699e779167a2fcfc116837b148cde5119d3cc3c4
Antoni <s33345@pjwstk.edu.pl>
2025-03-12 18:21:33 +0100

    Dodałem kolejną zmienną

--- Main.java
+++ Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int wiek = 20;
+        int liczba = 10;
+        System.out.println(wiek + liczba);
     }
 }
